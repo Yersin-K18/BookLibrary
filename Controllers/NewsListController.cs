3f5de232c2dc770b9d120697d9e02e5f4005e72e
Hoang3409 <nvh.02021995@gmail.com>
2022-12-26 09:46:02 +0700

    Add view #15

---
+++ Controllers/NewsListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookLibrary.Controllers
+{
+    public class NewsListController : Controller
+    {
+        // GET: NewList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

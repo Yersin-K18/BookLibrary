c1195e8cc1bd9615596d0726b5b17b7f6b43fe5c
Hoang3409 <nvh.02021995@gmail.com>
2022-12-30 20:46:18 +0700

    Add connect and Category model

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BookLibrary.Models
+{
+    public class CategoryModel
+    {
+        static BookLibraryEntities database = new BookLibraryEntities();
+
+        public List<Category> GetCategories()
+        {
+            return database.Categories.ToList();
+        }
+    }
+}

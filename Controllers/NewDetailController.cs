7799cdad523bdd7e8c7027086bb576867d01090a
ThanhSang <phamthanhsang12a2cl21@gmail.com>
2022-12-31 20:15:49 +0700

    Add newDetail and view

--- Controllers/NewDetailController.cs
+++ Controllers/NewDetailController.cs
@@ -6,9 +6,9 @@ using System.Web.Mvc;
 
 namespace BookLibrary.Controllers
 {
-    public class AuthorDetailController : Controller
+    public class NewDetailController : Controller
     {
-  
+        // GET: NewDetail
         public ActionResult Index()
         {
             return View();

ff6e08a3187a73d0ae63bb038b7712f634a98dbb
phu1110 <104994250+phu1110@users.noreply.github.com>
2023-01-07 17:10:33 +0700

    fix Layout v3 and Add Author and fix link Authordetail (#51)
    Co-authored-by: phu <phu3365@gmail,com>

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookLibrary.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

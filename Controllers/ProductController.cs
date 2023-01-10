2473ef8574a62c220f22a8b12c3c2468e125e766
vule2303 <107258990+vule2303@users.noreply.github.com>
2023-01-10 21:40:42 +0700

    62 particle product (#65)
    * update
    
    * Update: Category and products

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,16 @@ namespace BookLibrary.Controllers
 {
     public class ProductController : Controller
     {
+        BookLibraryEntities db = new BookLibraryEntities();
         // GET: Product
         public ActionResult Index()
         {
             return View();
         }
+        [ChildActionOnly]
+        public ActionResult AllCategory()
+        {
+            return PartialView("_getAllCategory", db.Categories);
+        }        
     }
 }
---
+++ Models/ProductsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using System.Web.Mvc;
+
+namespace BookLibrary.Models
+{
+    public class ProductsModel
+    {
+
+        static BookLibraryEntities db = new BookLibraryEntities();
+        static public List<Product> list_BestSellingProducts(int count)
+        {
+            
+            return db.Products.OrderByDescending(a => a.Name).Take(count).ToList();
+        }
+    }
+}

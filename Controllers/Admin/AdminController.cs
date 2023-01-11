92c36bf66fe600a030c0acc4e65141ae9ba6d689
Hoang3409 <nvh.02021995@gmail.com>
2023-01-11 10:40:57 +0700

    Done for manager, not css

---
+++ Controllers/Admin/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace BookLibrary.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                ViewBag.Message = "Must input data!";
+                return View();
+            }
+            if (username == "admin" && password == "1")
+            {
+                return View("Manage");
+            }
+            return View();
+        }
+
+        public ActionResult Manage()
+        {
+            return View();
+        }
+    }
+}

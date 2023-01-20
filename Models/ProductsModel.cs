ffb4bba677bc4bfaf4cbf5b573a4d7ab6a0ffb62
Hoang3409 <nvh.02021995@gmail.com>
2023-01-20 09:27:03 +0700

    Done index-1

--- Models/ProductsModel.cs
+++ Models/ProductsModel.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Management;
-using System.Web.Mvc;
 
 namespace BookLibrary.Models
 {
@@ -13,7 +9,6 @@ namespace BookLibrary.Models
         static BookLibraryEntities db = new BookLibraryEntities();
         static public List<Product> list_BestSellingProducts(int count)
         {
-            
             return db.Products.OrderByDescending(a => a.Name).Take(count).ToList();
         }
     }

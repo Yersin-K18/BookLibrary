fd1b050d0182fa07ef25752a0eea67706dfc3012
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2023-01-21 19:58:04 +0700

    Complete the form for Login and Register (#85)
    * Create view for login and register
    
    * complete the form for login
    
    Co-authored-by: tdquang <quangjackson995@gmail.com>

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookLibrary.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult LoginForm()
+        {
+            return View();
+        }
+    }
+}

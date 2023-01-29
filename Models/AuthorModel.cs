2024a91f1a40e99f496999f5491476f853a311cf
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2023-01-29 15:56:28 +0700

    Done partial top authors (#87)

---
+++ Models/AuthorModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BookLibrary.Models
+{
+    public class AuthorModel
+    {
+        static BookLibraryEntities db = new BookLibraryEntities();
+
+        static public List<Author> TopAuthors(int Number)
+        {
+            return db.Authors.OrderByDescending(i => i.BooksPublished)
+                             .Take(Number)
+                             .ToList();
+        }
+    }
+}

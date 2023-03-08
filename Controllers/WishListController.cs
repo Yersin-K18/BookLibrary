4494802b3590c39accedf073875f9991a8f7576e
HoangDSR <nvh.02021995@gmail.com>
2023-03-08 15:22:02 +0700

    Done wishlist but not implement all button

---
+++ Controllers/WishListController.cs
@@ -0,0 +1,34 @@
+﻿using BookLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace BookLibrary.Controllers
+{
+    public class WishListController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return PartialView("_WishList");
+        }
+        public ActionResult AddWishList(int id)
+        {
+            if (Session["WishList"] is null)
+            {
+                Session["WishList"] = new HashSet<Product>();
+            }
+            var temp = (HashSet<Product>)Session["WishList"];
+            temp.Add(ProductsModel.FindById(id));
+            return Index();
+        }
+        public void RemoveWishList(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public int NumberBookInWishList()
+        {
+            return ((HashSet<Product>)Session["WishList"]).Count;
+        }
+    }
+}

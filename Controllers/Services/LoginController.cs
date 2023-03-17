430f5b97b6b3a8d5bd9cc01a641218b0b54e051d
HoangDSR <nvh.02021995@gmail.com>
2023-03-18 01:28:19 +0700

    change .net framework 4.8, implement api login

--- Controllers/Services/LoginController.cs
+++ Controllers/Services/LoginController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookLibrary.Models;
 using System;
 using System.Web.Mvc;
 
@@ -7,9 +7,37 @@ namespace BookLibrary.Controllers.Services
     public class LoginController : Controller
     {
         [HttpPost]
-        public IActionResult Login()
+        public JsonResult Login(String username, string password)
         {
-            throw new NotImplementedException();
+            User user = UserModel.VerifyCredentials(username, password);
+            JsonResult response;
+            if (user is null)
+            {
+                response = new JsonResult()
+                {
+
+                    Data = new
+                    {
+                        message = "Failure",
+                        token = ""
+                    }
+                };
+            }
+            else
+            {
+                response = new JsonResult()
+                {
+                    Data = new
+                    {
+                        message = "Success",
+                        token = ""
+                    }
+                };
+            }
+            return response;
         }
+        // fetch("/Login/Login?username=admin&password=1", { "method": "POST"})
+        // .then(res => res.json())
+        // .then(res => console.log(res))
     }
 }

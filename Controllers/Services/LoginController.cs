667476318c45af7dcd2b21b4bce0d0bb860a2632
HoangDSR <nvh.02021995@gmail.com>
2023-03-18 00:02:05 +0700

    Init API Login, Using IActionResult
    IActionResult cũng giống như ActionResult nhưng IActionResult không đóng gói trực tiếp các kết quả trả về từ action mà chỉ trả về một đối tượng kết quả tùy thuộc vào loại kết quả được trả về.
    Một lợi ích của việc sử dụng IActionResult là cho phép bạn đơn giản hóa việc xây dựng kiến trúc của ứng dụng, giảm thiểu phụ thuộc giữa các thành phần và dễ dàng thay đổi kết quả trả về từ action mà không ảnh hưởng đến các class khác trong app.

---
+++ Controllers/Services/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Web.Mvc;
+
+namespace BookLibrary.Controllers.Services
+{
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public IActionResult Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
